11ec0a01f42d190a264fa483d5e4ce0f7fecd86f
Colton Bassett <coltonbassett@gmail.com>
2021-03-10 17:43:30 -0800

    small thing

--- src/components/characteroverview.tsx
+++ src/components/characteroverview.tsx
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
 		borderBottomRightRadius: '3.5rem', 
 		backgroundColor: '#232D38', 
 		boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
-		minHeight: '16.5rem'
+		minHeight: '17.75rem'
 	},
 	characterCardInnerContainer: {
 		padding: '0rem 0rem 1.5rem 0rem',
